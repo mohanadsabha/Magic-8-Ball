4f118c163952b6df21ec02cfd209400fbd3d30ea
Mohanad Abu Sabha <mohannad.naxol@gmail.com>
2022-07-25 04:29:20 +0300

    Initial Commit

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Mohanad on 7/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
